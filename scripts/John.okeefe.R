fa459c5a4bf75528610244e2b69adf0e350da573
cchambe12 <cchamberlain@g.harvard.edu>
2016-09-15 18:09:05 -0400

    Updated Script and Method Test Data

--- scripts/John.okeefe.R
+++ scripts/John.okeefe.R
@@ -24,17 +24,57 @@ ipak(packages)
 
 # Integrate Phenology Data and compare methodologies
 # Set Working Directory
-setwd("~/Documents/git/falsespring")
+setwd("~/Documents/git/springfreeze/input")
 
 budburst<-read.csv("hf003-06-mean-spp.csv",header=TRUE, sep=",")
 attach(budburst)
 
-d<- budburst %>%
+sp <- budburst %>%
   select(year, species, bb.jd) %>%
-  filter(species == c("COAL",""))
+  arrange(bb.jd) %>%
+  group_by(species, year)%>%
+  arrange(year) 
 
+spp<- c("PRSE", "AMSP", "POTR", "CRSP", "HAVI", "ACSA", "BEPA", "ACPE", "ACRU", "QURU", 
+                         "BEAL", "BELE") ## Species that were observed each year
 
-obs_bb<- budburst %>%
+d <- budburst %>%
+  select(year, species, bb.jd)%>%
+  filter(species %in% spp)
+
+df<- d %>%
+  select(species, bb.jd) %>%
+  arrange(bb.jd)
+
+obs_bb<- d %>%
   group_by(year)%>%
   summarise_each(funs(mean),bb.jd)%>%
   arrange(year)
+
+compare<- read.csv("method.test.csv", header=TRUE, sep=",")
+attach(compare)
+
+obs.npn<- obs_bb %>%
+  full_join(compare) %>%
+  select(year, bb.jd, bb_npn) %>%
+  arrange(year) %>%
+  filter(year>=1990) %>%
+  filter(year<2015)
+
+## Species that are more comparable to lilacs and honeysuckle ##
+sm.sp<- c("PRSE", "POTR", "AMSP", "CRSP", "HAVI", "ACSA", "BEPA", "ACPE")
+small<- budburst%>%
+  select(year, species, bb.jd) %>%
+  filter(species %in% sm.sp)
+condensed<- small %>%
+  group_by(year)%>%
+  summarise_each(funs(mean),bb.jd)%>%
+  arrange(year)
+
+sm.comp<- condensed %>%
+  full_join(compare) %>%
+  select(year, bb.jd, bb_npn, last_frz) %>%
+  arrange(year) %>%
+  filter(year>=1990) %>%
+  filter(year<2015)
+
