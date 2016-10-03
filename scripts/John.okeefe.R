9b9c1729fabde90824f6bc13fe8eb1d5bdfb23c2
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2016-10-03 18:05:20 -0400

    Made presentation and updated scripts

--- scripts/John.okeefe.R
+++ scripts/John.okeefe.R
@@ -17,7 +17,7 @@ ipak <- function(pkg){
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
-packages <- c("ggplot2", "dplyr", "lattice", "tidyr")
+packages <- c("ggplot2", "dplyr", "lattice", "tidyr", "broom")
 ipak(packages)
 
 # Integrate Phenology Data and compare methodologies
@@ -93,15 +93,37 @@ FSI.table<- method %>%
   select(year, FSI_npn, FSI_sm, FSI_obs, FSI_cam) %>%
   filter(year>=2008) %>%
   filter(year<2015)
-
+FSI.long<- method%>%
+  select(year,FSI_npn,FSI_sm, FSI_obs)%>%
+  filter(year>=2001)%>%
+  filter(year<2015)
 blend<-FSI.table %>% 
-  gather(Methodologies, FSI, -year) %>%
+  gather(Method, FSI, -year) %>%
   arrange(year)
+blend.long<-FSI.long%>%
+  gather(Method,FSI,-year)%>%
+  arrange(year)
+
+
+boxplot(data=blend, FSI~Method, xlab="Method", ylab="FSI")
+boxplot(data=blend.long, FSI~Method)
 
 methodplot<-ggplot(blend, aes(year, FSI)) + xlab("Year") +
   ylab("False Spring Index") +
-  geom_point(aes(col=Methodologies))
+  geom_point(aes(col=Method)) + geom_smooth()
 plot(methodplot)
+
+longplot<-ggplot(blend.long,aes(year,FSI)) +xlab("Year") +
+  ylab("False Spring Index") + scale_x_continuous(limits = c(2001,2014),breaks=seq(2001,2014,1)) +
+  geom_point(aes(col=Method)) + geom_smooth()
+plot(longplot)
+
+fit<-glm(FSI~Method + year, data=blend)
+fit
+tidy(fit)
+head(augment(fit))
+glance(fit)
+plot(fit)
 
 npn_sm <- method %>%
   select(year, FSI_npn, FSI_sm, FSI_obs, FSI_cam) %>%
@@ -113,3 +135,72 @@ summary(regression)
 
 pearson<-cor(FSI.table, method="pearson")
 pearson
+tidy(pearson)
+glance(pearson)
+pearson.long<-cor(FSI.long, method="pearson")
+pearson.long
+
+install.packages("xtable")
+library(xtable)
+pears<-xtable(pearson, caption="Pearson Correlation Coefficients shown comparing the strength of association between the FSI values calculated across all three methodologies.")
+write.csv(pears, file ="cortable.csv")
+long<-xtable(pearson.long, caption="Pearson Correlation Coefficients shown comparing the strength of association between the FSI values calculated across all three methodologies.")
+write.csv(long, file ="cor.long.csv")
+
+## Attempting Bayesian Correlation
+library(BayesFactor)
+
+jzs_corbf=function(r,n){
+  int=function(r,n,g){
+    (1+g)^((n-2)/2)*(1+(1-r^2)*g)^(-(n-1)/2)*
+      g^(-3/2)*exp(-n/(2*g))};
+  bfl0=sqrt((n/2))/gamma(1/2)*integrate(int,lower=0,upper=Inf,r=r,n=n)$value; 
+  return(bfl0)}
+r <- 0.9497443
+n <- 5
+jzs_corbf(r,n)
+
+##Bayes Factor ttest - not sure how to interpret results...
+npn.sm<-method%>%
+  select(year,FSI_npn,FSI_sm)%>%
+  filter(year>=2001)%>%
+  filter(year<2015)%>%
+  gather(Method, FSI, -year) %>%
+  arrange(year)
+npn.obs<-method%>%
+  select(year,FSI_npn,FSI_obs)%>%
+  filter(year>=2001)%>%
+  filter(year<2015)%>%
+  gather(Method, FSI, -year) %>%
+  arrange(year)
+npn.cam<-FSI.table%>%
+  select(year,FSI_npn,FSI_cam)%>%
+  gather(Method, FSI, -year) %>%
+  arrange(year)
+sm.obs<-method%>%
+  select(year,FSI_sm,FSI_obs)%>%
+  filter(year>=2001)%>%
+  filter(year<2015)%>%
+  gather(Method, FSI, -year) %>%
+  arrange(year)
+sm.cam<-FSI.table%>%
+  select(year,FSI_sm,FSI_cam)%>%
+  gather(Method, FSI, -year) %>%
+  arrange(year)
+obs.cam<-FSI.table%>%
+  select(year,FSI_obs,FSI_cam)%>%
+  gather(Method, FSI, -year) %>%
+  arrange(year)
+
+t.test(FSI~Method, data=npn.sm)
+ttestBF(formula=FSI~Method, data=npn.sm)
+t.test(FSI~Method, data=npn.obs)
+ttestBF(formula=FSI~Method, data=npn.obs)
+t.test(FSI~Method, data=npn.cam)
+ttestBF(formula=FSI~Method, data=npn.cam)
+t.test(FSI~Method, data=sm.obs)
+ttestBF(formula=FSI~Method, data=sm.obs)
+t.test(FSI~Method, data=sm.cam)
+ttestBF(formula=FSI~Method, data=sm.cam)
+t.test(FSI~Method, data=obs.cam)
+ttestBF(formula=FSI~Method, data=obs.cam)
