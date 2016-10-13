29be9a4c8759ba321199efa4a81e78f7771317a2
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2016-10-13 10:19:45 -0400

    bb to leaf out script update

--- scripts/bb.leafout.R
+++ scripts/bb.leafout.R
@@ -15,8 +15,21 @@ library(tidyr)
 library(ggplot2)
 
 # Set Working Directory
-setwd("~/Documents/git/input/springfreeze")
-phenology<-read.csv("individual_phenometrics_data.csv",header=TRUE, sep=",")
+setwd("~/Documents/git/springfreeze")
+phenology<-read.csv("input/individual_phenometrics_data.csv",header=TRUE, sep=",")
 attach(phenology)
 
+phases<-c("371","483")
 
+phenology<-phenology%>%
+  select(Genus, Species, Phenophase_ID, First_Yes_DOY, First_Yes_Year, Latitude, Longitude)%>%
+  unite(species, Genus, Species, sep="_") %>%
+  filter(Phenophase_ID %in% phases) %>%
+  rename(Year = First_Yes_Year)
+  
+spp<-as.data.frame(table(phenology$species))
+
+d<-phenology%>%
+  filter(Year=="2010")%>%
+  arrange(species)
+  
