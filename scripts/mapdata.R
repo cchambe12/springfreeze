671752a488b48c26f921b10a6a6fa6cfb9efc81f
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2016-11-11 08:13:14 -0500

    updated ggplot2 map

---
+++ scripts/mapdata.R
@@ -0,0 +1,49 @@
+## 9 November 2016 - Cat
+## Attempt to Map out NPN data for Timeline data
+
+# Install Packages
+install.packages(c("stringr","maps","mapdata"))
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+library(dplyr)
+library(tidyr)
+
+# Upload map of US
+usa <- map_data("usa")
+canada <- map_data("worldHires", "Canada")
+mexico <- map_data("worldHires", "Mexico")
+states <- map_data("state")
+alaska <- subset(states, region %in% "alaska")
+
+gg1<- NAmap <- ggplot() + geom_polygon(data = usa, 
+                                       aes(x=long, y = lat, group = group), 
+                                       fill = "white", 
+                                       color="black") +
+  geom_polygon(data = canada, aes(x=long, y = lat, group = group), 
+               fill = "white", color="black") + 
+  geom_polygon(data = mexico, aes(x=long, y = lat, group = group), 
+               fill = "white", color="black") + 
+  geom_polygon(data = mexico, aes(x=long, y = lat, group = group), 
+                                                             fill = "white", color="black") 
+
+# Set Working Directory
+setwd("~/Documents/git/springfreeze")
+phenology<-read.csv("input/condensed_phenometrics.csv",header=TRUE)
+attach(phenology)
+
+phases<-c("Budburst","Leaves")
+
+pheno<-phenology%>%
+  select(Genus, Species, Individual_ID, Phenophase_Description, First_Yes_DOY, First_Yes_Year, Latitude, Longitude) %>%
+  unite(species, Genus, Species, sep="_") %>%
+  filter(Phenophase_Description %in% phases) %>%
+  rename(Year = First_Yes_Year) %>%
+  filter(Longitude != 0)
+
+# Plot Points
+gg1 + 
+  geom_point(data = pheno, aes(x = Longitude, y = Latitude), color = "black", size = 3) +
+  geom_point(data = pheno, aes(x = Longitude, y = Latitude), color = "red", size = 3)
+
