9c2bfe2b277f04b117a4859a77e311059c5249f9
cchambe12 <cchamberlain@g.harvard.edu>
2017-03-09 19:18:04 -0500

    analysis for latitude data

---
+++ scripts/Lat.Analysis.R
@@ -0,0 +1,54 @@
+# 9 March 2017
+# Cat
+# Attempt to analyze the latitudinal gradient data
+# See if there is a relationship with latitude and false springs over time
+# Is there a shift with climate change?
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(arm)
+library(rstanarm)
+
+# Set Working Directory
+setwd("~/Documents/git/springfreeze/input")
+df<-read.csv("all.lat.csv", header=TRUE)
+mat<-read.csv("matrix.lat.csv",header=TRUE, check.names=FALSE)
+
+#Clean matrix dataframe and join dataframes
+mat<-mat %>%
+  gather(year, fs, -Station)
+df2<-mat%>%
+  filter(mat$year!="Total")%>%
+  filter(Station!="Total")
+d<-full_join(df, mat, by="Station")
+d<-d %>%
+  filter(Station!="Total")%>%
+  filter(year!="Total")
+d$frost<-ifelse(d$fs==0, NA, d$fs)
+d<-na.omit(d)
+d<-dplyr::select(d,-hf.gdd, -fs)
+fs<-as.data.frame(table(d$year))%>%
+  rename(year=Var1)%>%
+  rename(fs=Freq)
+lat<-as.data.frame(table(d$Latitude))%>%
+  rename(Latitude=Var1)%>%
+  rename(lat=Freq) 
+lat$Latitude<-as.numeric(as.character(lat$Latitude))
+d1<-full_join(d,fs,by="year")
+d1<-full_join(d1,lat,by="Latitude")
+d1$year<-as.numeric(as.character(d1$year))
+
+# Initial Plots
+ggplot(d1, aes(x=year, y=fs)) + geom_point(aes(color=Latitude))
+ggplot(d1, aes(x=Latitude, y=lat)) + geom_point(aes(color=year))
+
+# relationships?
+mod<-lm(fs~year,data=d1)
+mod1<-lm(lat~Latitude + year:Latitude, data=d1)
