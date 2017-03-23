c6a68920ee0613e25ca4331632b3af2f442b108d
cchambe12 <cchamberlain@g.harvard.edu>
2017-03-23 13:43:34 -0400

    working on treespotters figure and npn data

--- scripts/ts.timeline.R
+++ scripts/ts.timeline.R
@@ -15,6 +15,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(arm)
+library(data.table)
 # Set Working Directory
 setwd("~/Documents/git/springfreeze")
 phenology<-read.csv("input/treespotters.timeline.csv",header=TRUE)
@@ -28,17 +29,6 @@ pheno<-phenology%>%
   rename(Year = First_Yes_Year) 
 
 # Make dataframe that includes Risk for each species
-y0<-pheno%>%
-  filter(Year=="2015")%>%
-  group_by(species, Individual_ID, Phenophase_Description)%>%
-  arrange(Individual_ID) %>%
-  filter(row_number()==1) %>%
-  spread(Phenophase_Description, First_Yes_DOY)
-y0<-na.omit(y0)
-y0$Risk <- y0$Leaves - y0$Budburst
-y0<-filter(y0, Risk > 0)
-y0<-filter(y0, Risk < 31)
-
 y1<-pheno%>%
   filter(Year=="2016")%>%
   group_by(species, Individual_ID, Phenophase_Description)%>%
@@ -48,9 +38,48 @@ y1<-pheno%>%
 y1<-na.omit(y1)
 y1$Risk <- y1$Leaves - y1$Budburst
 y1<-filter(y1, Risk > 0)
-y1<-filter(y1, Risk < 31)
+y1<-ungroup(y1)
 
-dat<-full_join(y0,y1)
+y1$mean<-ave(y1$Budburst, y1$species)
+y1$sd<-ave(y1$Budburst, y1$species, FUN=sd)
+y1$mean.leaf<-ave(y1$Leaves, y1$species)
+y1$sd.leaf<-ave(y1$Leaves, y1$species, FUN=sd)
+y<- y1 %>%
+  dplyr::select(species, Budburst, Leaves, Risk, mean, sd, mean.leaf, sd.leaf) %>%
+  gather(Phenophase, DOY, -species, -Risk, -mean, -sd, -mean.leaf, -sd.leaf)
+y<-y %>%
+  group_by(species, mean, Phenophase)%>%
+  arrange(species)%>%
+  filter(row_number()==1)%>%
+  ungroup(y)
+
+y3 = melt(setDT(y), 
+           measure.vars = patterns("mean", "mean.leaf"), 
+           variable.name = "Mean")
+y3[, Mean := factor(Mean, labels = c("bud","leaf"))]
+y3<-rename(y3, Budburst=value1, Leaves=value2)
+y3<- y3 %>%
+  dplyr::select(-Mean)%>%
+  arrange(Phenophase)%>%
+  gather(pheno, Mean, -species, -Risk, -Phenophase, -DOY, -sd, -sd.leaf)
+y3$Phenophase<- ifelse(y3$Phenophase==y3$pheno, y3$Phenophase, NA)
+y3<-na.omit(y3)
+y3 <- y3[-c(12:22), ]
+y3<- dplyr::select(y3, -pheno)
+
+y2 = melt(setDT(y3), 
+          measure.vars = patterns("sd", "sd.leaf"), 
+          variable.name = "sd")
+y2[, sd := factor(sd, labels = c("bud","leaf"))]
+y2<-rename(y2, Budburst=value1, Leaves=value2)
+y2<- y2 %>%
+  dplyr::select(-sd)%>%
+  arrange(Phenophase)%>%
+  gather(pheno, sd, -species, -Risk, -Phenophase, -DOY, -Mean)
+y2$Phenophase<- ifelse(y2$Phenophase==y2$pheno, y2$Phenophase, NA)
+y2<-na.omit(y2)
+y2 <- y2[-c(12:22), ]
+y2<- dplyr::select(y2, -pheno)
 
 bud<- y1 %>%
   dplyr::select(species, Budburst) %>%
@@ -64,21 +93,32 @@ leaves<- y1 %>%
   summarise_each(funs(mean), Leaves) %>%
   arrange(species)
 
+
 basic<- full_join(bud, leaves)
 basic$Risk<- basic$Leaves - basic$Budburst
 
-basic$code <- reorder(basic$species, basic$Budburst)
+#basic$code <- reorder(basic$species, basic$Budburst)
 
-ts.timeline<-ggplot((basic), aes(x=Budburst, y=code), stat="identity") + geom_point(aes(x= basic$Budburst)) + 
+#df<-basic %>%
+  #gather(Phenophase, DOY, -Risk, -species)
+#df$sd<- df%>%
+  #group_by(species)%>%
+  #summarise_each(funs(sd), DOY)
+
+
+ts.timeline<-ggplot((basic), aes(x=Budburst, y=species), stat="identity") + geom_point(aes(x= basic$Budburst)) + 
   geom_segment(aes(y = species, yend = species, x = Budburst, xend = Leaves)) + 
   geom_point(aes(x=basic$Leaves)) + theme(legend.position="none") + geom_point(aes(col=species)) + xlab("Budburst to Leaf Out") +
-  ylab("Species")
+  ylab("Species") 
 plot(ts.timeline)
+
+ts<-ggplot((y2), aes(x=species, y=DOY)) + geom_point(aes(col=Phenophase)) + 
+  xlab("Day of Year") + ylab("Species") + geom_errorbar(aes(ymin=DOY-sd, ymax=DOY+sd, col=Phenophase), width=.0)
+plot(ts)
+
 
 ggplot((basic), aes( x= Budburst, y=Risk)) + geom_smooth(method="lm", se=FALSE) + geom_point(aes(col=species))
 
 lmodel<-lm(Risk~Budburst,data=basic)
-display(lmodel)
-
-basic$sd<-sd(basic$Risk)
-basic$mean<-mean(basic$Risk)
+lme1<-lm(Risk~species, data=y2)
+display(lme1)
