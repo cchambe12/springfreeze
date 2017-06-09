0af2768ffc0a08b4f80bd09420ee76baae64f1c0
cchambe12 <cchamberlain@g.harvard.edu>
2017-06-09 11:09:53 -0400

    so many updates to hf graph...

--- scripts/hf.timeline.R
+++ scripts/hf.timeline.R
@@ -38,20 +38,12 @@ w$count <- ave(
 w<-rename(w, year=Year)
 w<-rename(w, doy=JD)
 # Determine HF false springs based on data
-w$frz<- ifelse((w$AirTMin<=-4), "freeze", "thaw")
-w$fs<- ifelse((w$count >= 100 & w$frz == "freeze" & w$count<=400), TRUE, NA)
-
-w.count<- dplyr::select(w, year, fs)
-w.count<-na.omit(w.count)
-w.count<-as.data.frame(table(w.count$year))
-
-climate<-ggplot((w), aes(x=JD, y=AirT)) + xlab("Day of Year") + ylab("Mean Daily Temperature (C)") +
-  geom_point(aes(col=factor(Year)))+
-  geom_line(aes(x=JD, y=AirT, col=factor(Year))) + scale_shape_manual(values=c("#999999", "#56B4E9"),
-                                                                     name="Year") +
-  scale_color_manual(labels = c("2010", "2014"), values = c("purple3", "royalblue3")) +
-  labs(color="Year")
-climate
+w$ten<-ifelse((w$doy >=107 & w$doy <= 145 & w$year == 2010), w$count, 0)
+w$four<-ifelse((w$doy >=130 & w$doy <= 158 & w$year == 2014), w$count, 0)
+w$grow<-w$ten + w$four
+w<-dplyr::select(w, -ten, -four)
+w$grow<-ifelse((w$grow==0), NA, w$grow)
+w<-na.omit(w)
 
 # Add Risk and only Two Years
 years<-c("2010", "2014")
@@ -65,26 +57,31 @@ df<- timeline %>%
 df$si[df$year=="2010"] <- "early"
 df$si[df$year=="2014"] <- "late"
 #df<-na.omit(df)
-w<-w%>%filter(doy>=100)
-gdd<- ggplot((w), aes(doy, count, col=factor(year))) + geom_line(aes(x=doy, y=count, col=factor(year)), size=4) + ylab("Growing Degree Days") + xlab("Day of Year") + labs(col="Year")
-airT<-ggplot((w), aes(doy, AirT, col=factor(year))) + ylab("Average Daily Air Temperature (C)") + xlab("Day of Year") + labs(col="Year") +
-  geom_smooth(aes(col=factor(year)), method="loess")
+
+gdd<- ggplot((w), aes(doy, grow, col=factor(year))) + geom_line(aes(x=doy, y=grow, col=factor(year)), size=4) + 
+  ylab("Growing Degree Days") + xlab("Day of Year") + labs(col="Year") 
+
 # Make a graph!
-df$code <- reorder(df$species, df$bb.jd)
+df$code <- reorder(df$species, df$year)
+df$ord<- reorder(df$code, df$bb.jd)
+
 
 df$bb.jd<-as.numeric(as.character(df$bb.jd))
 df$l75.jd<-as.numeric(as.character(df$l75.jd))
 
-hf<-ggplot(df, aes(x = code,ymin = bb.jd, ymax = l75.jd, group=interaction(species, year) )) +
+hf<-ggplot(df, aes(x=ord,ymin = bb.jd, ymax = l75.jd, group=interaction(species, year) )) +
   geom_point(aes(y=bb.jd, col="forestgreen"), position = position_dodge(.5)) + geom_point(aes(y=l75.jd, col="darkgreen"), position = position_dodge(.5)) +
-  geom_linerange(aes(x = code,ymin = bb.jd, ymax = l75.jd, col=factor(year)), position=position_dodge(.5)) +  ylab("Day of Year") +
+  geom_linerange(aes(x=ord,ymin = bb.jd, ymax = l75.jd, col=factor(year)), position=position_dodge(.5)) +  ylab("Day of Year") +
   scale_color_manual(labels = c("2010", "2014", "Leafout", "Budburst"), values = c("#F8766D", "#00BFC4", "green4", "darkolivegreen3")) +
-  xlab("Species") +coord_flip() + labs(color="Phenophase and Year", title="Harvard Forest Data")
+  xlab("Species") +coord_flip() + labs(color="Phenophase and Year")
 plot(hf)
 
 grid.newpage()
 grid.draw(rbind(ggplotGrob(hf), ggplotGrob(gdd), size="first"))
 
+
+df$mean<-ave(df$risk, df$year)
+df$stand<-ave(df$risk, df$year, FUN = sd)
 
 prep<-df%>%
   gather("phenophase", "doy", bb.jd, l75.jd)
@@ -103,6 +100,7 @@ overlay
 
 
 ## Analysis
+
 mod.gdd<-glm(risk~count, data=grand)
 display(mod.gdd)
 
