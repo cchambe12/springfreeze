b9df235f25fef7f395218332b5dfb9a124488cb4
cchambe12 <cchamberlain@g.harvard.edu>
2019-03-27 17:24:03 -0400

    cleaning repo and final tweaks to manuscript

---
+++ scripts/zarchive/DVR_diffPlot.R
@@ -0,0 +1,87 @@
+## Most updated script for Dan's experiment plots
+# Cat - 1 November 2017
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Set Working Directory
+setwd("~/Documents/git/springfreeze")
+d<-read.csv("input/Budburst.clean.csv",header=TRUE)
+
+tx<-c("CL0", "WL1")
+dx<- d %>%
+  dplyr::select(ind, treatcode, lday, bday, site) %>%
+  filter(treatcode %in% tx)
+  
+## Omit Viburnum due to budburst date issues
+dx<-na.omit(dx)
+dx$species<-substr(dx$ind, 1, 6)
+dx<-dx%>%filter(species!="VIBCAS")%>%filter(species!="VIBLAN") # all entries for two species have the same budburst and leafout day, removed because probably from error
+small.spp<-dx %>% dplyr::select(species, treatcode) %>% filter(treatcode=="WL1")
+spp<-unique(small.spp$species)
+dx<-dx%>% filter(species %in% spp)
+dx<-dplyr::select(dx, -ind, -site)
+
+
+### Start determining risk for each treatment
+dxx<-dx
+dxx$risk<-dxx$lday-dxx$bday
+dxx$dvr<-ave(dxx$risk, dxx$species, dxx$treatcode)
+dxx$dvr.sd<-ave(dxx$risk, dxx$species, dxx$treatcode, FUN=sd)/sqrt(length(unique(dxx$dvr)))
+dxx<-dplyr::select(dxx, species, treatcode, dvr, dvr.sd)
+dxx<-dxx[!duplicated(dxx),]
+dxx$tx.sd<-paste(dxx$treatcode, "se", sep="_")
+dm<-dxx%>%dplyr::select(species, treatcode, dvr)%>%spread(treatcode, dvr)
+ds<-dxx%>%dplyr::select(species, tx.sd, dvr.sd)%>%spread(tx.sd, dvr.sd)
+dxx<-inner_join(dm, ds)
+dxx$diff<-dxx$CL0-dxx$WL1
+dxx$diff.sd<-sqrt((dxx$CL0_se)^2+(dxx$WL1_se)^2) 
+df<-dx%>%filter(treatcode=="WL1")%>%dplyr::select(treatcode, bday, species)
+df$bday<-ave(df$bday, df$species, df$treatcode)
+df<-df[!duplicated(df),]
+
+
+df$species<-ifelse(df$species=="ILEMUC", "I. mucronata", df$species)
+df$species<-ifelse(df$species=="ACEPEN", "A. pensylvanicum", df$species)
+df$species<-ifelse(df$species=="BETALL", "B. alleghaniensis", df$species)
+df$species<-ifelse(df$species=="BETPAP", "B. papyrifera", df$species)
+df$species<-ifelse(df$species=="ACERUB", "A. rubrum", df$species)
+df$species<-ifelse(df$species=="POPGRA", "P. grandidentata", df$species)
+df$species<-ifelse(df$species=="QUERUB", "Q. rubra", df$species)
+df$species<-ifelse(df$species=="ACESAC", "A. saccharum", df$species)
+df$species<-ifelse(df$species=="FAGGRA", "F. grandifolia", df$species)
+
+df$code<-reorder(df$species, df$bday)
+dxx$code<-df$code
+
+#write.csv(dxx, file="~/Documents/git/springfreeze/output/diffplot.csv", row.names = FALSE)
+
+diff<-ggplot(dxx, aes(x=factor(code), y=diff)) + geom_point() + 
+    geom_linerange(aes(ymin=diff-diff.sd, ymax=diff+diff.sd), alpha=0.3) + 
+  ylab(expression(Delta*" in DVR between treatments")) + coord_cartesian(ylim=c(0,15)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title.x=element_blank(),
+        axis.text.x = element_text(face = "italic", angle=45, vjust=0.5), axis.text=element_text(size=10))
+plot(diff)
+  
+
+ggplot(dxx, aes(x=CS0, y=WL1)) + geom_point(aes(size=diff), shape=21) + 
+  geom_linerange(aes(ymin=WL1-WL1_se, ymax=WL1+WL1_se), alpha=0.3) +
+  geom_errorbarh(aes(xmax = CS0+CS0_se, xmin = CS0-CS0_se, height = 0), alpha=0.3) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  geom_text(aes(label=code), vjust=2) + xlab("DVR with weak treatment effects") + 
+  ylab("DVR with strong treatment effects") + 
+  scale_size_continuous(name=expression(Delta*" in DVR"))
+
+
+
+
+
+
+
---
+++ scripts/zarchive/HardinessFig.R
@@ -0,0 +1,69 @@
+### Make a new chart that shows the frost hardiness before, during and after dormancy
+## Hardiness graph
+# 8 February 2018 - Cat
+
+# Load Libraries
+library(ggplot2)
+
+month<- 1:9
+hrdy<-data.frame(month, hardy=NA, phase=NA)
+hrdy$hardy<-c(-8, -12, -18, -25, -30, -25, -10, -2, -3)
+hrdy$phase<-c("Paradormancy","Paradormancy", "Endodormancy", "Endodormancy", "Endodormancy", "Ecodormancy",
+              "Ecodormancy", "Budburst", "Leaf Expansion")
+
+ggplot(hrdy, aes(x=month, y=hardy)) +   
+  scale_x_continuous(position = "top", breaks=c(1,2,3,4,5,6,7,8, 9),
+                     labels=c("Sept", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May")) +
+  geom_point(aes(col=phase), col=c("sienna4","sienna4", "purple4", "purple4", "purple4", "navyblue", "navyblue", "green4", "yellowgreen")) +
+  geom_curve(aes(x = 1, y = -8, xend = 2, yend = -12), data = hrdy, col="sienna4", curvature =0.02) +
+  geom_curve(aes(x = 2, y = -12, xend = 3, yend = -18), data = hrdy, col="sienna4", curvature =-0.02) +
+  geom_curve(aes(x = 3, y = -18, xend = 4, yend = -25), data = hrdy, col="purple3", curvature = -0.02) +
+  geom_curve(aes(x = 4, y = -25, xend = 5, yend = -30), data = hrdy, col="purple3", curvature = 0.05) +
+  geom_curve(aes(x = 5, y = -30, xend = 6, yend = -25), data = hrdy, col="purple3", curvature = 0.05) +
+  geom_curve(aes(x = 6, y = -25, xend = 7, yend = -10), data = hrdy, col="navyblue", curvature = 0.02) +
+  geom_curve(aes(x = 7, y = -10, xend = 8, yend = -2), data = hrdy, col="green4", curvature = -0.1) +
+  geom_curve(aes(x = 8, y = -2, xend = 9, yend = -3), data = hrdy, col="yellowgreen", curvature = -0.1) +
+  scale_y_continuous(breaks=c(2, 0, -5, -10, -15, -20, -25, -30, -35)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  xlab("Month") + ylab("Cold Hardiness (°C)") + 
+  annotate("text", x = 2.25, y = -11.5, label = "Paradormancy", angle=318, size=3) +
+  annotate("text", x = 4.15, y = -24, label = "Endodormancy", angle=310, size=3) +
+  annotate("text", x = 6.45, y = -15.35, label = "Ecodormancy", angle=70, size=3) +
+  annotate("text", x = 8, y = -1, label = "Budburst", size=3) +
+  annotate("text", x = 9, y = -4, label = "Leafout", size=3)
+
+ggplot(hrdy, aes(x=month, y=hardy)) +   
+  scale_x_continuous(position = "top", breaks=c(1,2,3,4,5,6,7,8, 9),
+                     labels=c("Sept", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May")) +
+  geom_point(aes(col=phase)) +
+  geom_curve(aes(x = 1, y = -8, xend = 2, yend = -12), data = hrdy, col="sienna4", curvature =0.02) +
+  geom_curve(aes(x = 2, y = -12, xend = 3, yend = -18), data = hrdy,col="sienna4", curvature =-0.02) +
+  geom_curve(aes(x = 3, y = -18, xend = 4, yend = -25), data = hrdy,col="purple3", curvature = -0.02) +
+  geom_curve(aes(x = 4, y = -25, xend = 5, yend = -30), data = hrdy,col="purple3", curvature = 0.05) +
+  geom_curve(aes(x = 5, y = -30, xend = 6, yend = -25), data = hrdy,col="purple3", curvature = 0.05) +
+  geom_curve(aes(x = 6, y = -25, xend = 7, yend = -10), data = hrdy,col="navyblue", curvature = 0.02) +
+  geom_curve(aes(x = 7, y = -10, xend = 8, yend = -2), data = hrdy,col="green4", curvature = -0.1) +
+  geom_curve(aes(x = 8, y = -2, xend = 9, yend = -3), data = hrdy,col="yellowgreen", curvature = -0.1) +
+  scale_y_continuous(breaks=c(2, 0, -5, -10, -15, -20, -25, -30, -35)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        legend.key=element_rect(fill="white")) +
+  xlab("Month") + ylab("Cold Hardiness") + 
+  scale_color_manual(values=c("green4", "purple4", "navyblue","yellowgreen" , "sienna4"),
+                     labels=c("Budburst", "Endodormancy", "Ecodormancy","Leaf Expansion","Paradormancy")) +
+  labs(color="Phenophase")
+
+ggplot(hrdy, aes(x=month, y=hardy)) +   
+  scale_x_continuous(position = "top", breaks=c(1,2,3,4,5,6,7,8, 9),
+                     labels=c("Sept", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May")) +
+  geom_curve(aes(x = 1, y = -8, xend = 2, yend = -12), data = hrdy, curvature =0.02) +
+  geom_curve(aes(x = 2, y = -12, xend = 3, yend = -18), data = hrdy, curvature =-0.02) +
+  geom_curve(aes(x = 3, y = -18, xend = 4, yend = -25), data = hrdy, curvature = -0.02) +
+  geom_curve(aes(x = 4, y = -25, xend = 5, yend = -30), data = hrdy, curvature = 0.05) +
+  geom_curve(aes(x = 5, y = -30, xend = 6, yend = -25), data = hrdy, curvature = 0.05) +
+  geom_curve(aes(x = 6, y = -25, xend = 7, yend = -10), data = hrdy, curvature = 0.02) +
+  geom_curve(aes(x = 7, y = -10, xend = 8, yend = -2), data = hrdy, curvature = -0.1) +
+  geom_curve(aes(x = 8, y = -2, xend = 9, yend = -3), data = hrdy, curvature = -0.1) +
+  scale_y_continuous(breaks=c(2, 0, -5, -10, -15, -20, -25, -30, -35)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  xlab("Month") + ylab("Cold Hardiness (°C)")
+
